2b8f414a7b33f2039a30bfbab1159c8137710dea
Devin Wulf <devinlwulf@gmail.com>
2022-03-29 00:23:34 -0500

    create test files, css, app, lib files

---
+++ app.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const Employee = require('./lib/employee');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager');
